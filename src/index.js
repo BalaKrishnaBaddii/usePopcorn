6dde506391ac014a8f2f7f61bbd286a483e9643f
BalaKrishnaBaddii <bkrishna1114@gmail.com>
2024-06-19 16:11:53 +0530

    star rating component created

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import react from "react";
 import reactDom from "react-dom/client";
 import App from "./App";
+import StarRatings from "./StarRatings";
 
 const root = reactDom.createRoot(document.getElementById("root"));
 
 root.render(
   <react.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRatings maxRating={10} />
   </react.StrictMode>
 );
