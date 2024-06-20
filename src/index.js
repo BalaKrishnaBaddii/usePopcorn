50ff81bc733602cf6306b831303f21709d0e90d0
BalaKrishnaBaddii <bkrishna1114@gmail.com>
2024-06-20 11:01:47 +0530

    Rating Star componenet created

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import react from "react";
+import react, { useState } from "react";
 import reactDom from "react-dom/client";
 import App from "./App";
 import StarRatings from "./StarRatings";
@@ -7,7 +7,31 @@ const root = reactDom.createRoot(document.getElementById("root"));
 
 root.render(
   <react.StrictMode>
-    {/* <App /> */}
-    <StarRatings maxRating={10} />
+    <App />
+    <Rating />
+
+    {/* <StarRatings
+      maxRating={5}
+      messages={["Terriable", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={1}
+    /> */}
+    {/* <StarRatings maxRating={5} color="red" size={30} defaultRating={3} /> */}
   </react.StrictMode>
 );
+
+function Rating() {
+  const [movieRating, SetMovieRating] = useState(0);
+  console.log(movieRating);
+  return (
+    <>
+      <StarRatings
+        size={48}
+        color="green"
+        maxRating={5}
+        onRating={SetMovieRating}
+        messages={["Flop", "Bad", "Average", "Good", "Very Good"]}
+      />
+      <p>This Movie has reated {movieRating} Rating</p>
+    </>
+  );
+}
